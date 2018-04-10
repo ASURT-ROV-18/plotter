adc0846b2d830639710ae5cd0c6e40b5219e9342
GinaJemy <jemysalib@hotmail.com>
2018-04-10 19:11:25 +0200

    first commit

---
+++ dataplot.py
@@ -0,0 +1,44 @@
+import socket
+import sys
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.interpolate import spline
+# Create a UDP socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+# Bind the socket to the port
+server_address = ('localhost', 10000)
+#print('starting up on {} port {}'.format(*server_address))
+sock.bind(server_address)
+data = 'llll DATA: 9,-8,0,5.4,7,23,5,7,-1,4,3,6,12,4,12,8'
+
+while True:
+
+    #data, address = sock.recvfrom(4096)
+    if ('DATA:' not in data):
+        continue
+    else:
+        #data = data.decode
+        start = data.find("DATA:")
+        substring = data[start+5:]
+        vect = [x.strip() for x in substring.split(',')]
+        print (vect)
+        vect = [float(numeric_string) for numeric_string in vect]
+
+        #p = plt.plot(x,vect)
+        #plt.show(p)
+        xnew = np.linspace(1,16,200)
+
+        power_smooth = spline(range(1,17),vect,xnew)
+
+        plt.plot(xnew,power_smooth)
+        font = {'family': 'serif',
+                'color': 'black',
+                'weight': 'normal',
+                'size': 14,
+                }
+        plt.scatter(range(1,17), vect)
+        plt.grid(axis='y')
+        plt.xlabel('Time (sec)', fontdict=font)
+        plt.ylabel('Amplitude', fontdict=font)
+        plt.show()
